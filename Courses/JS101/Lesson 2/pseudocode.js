1b2e2920bc58c0139f907a44382148c6cb7b7d6b
Christian <christian.james.fisk@gmail.com>
2023-04-27 21:44:31 +1000

    Study

---
+++ Courses/JS101/Lesson 2/pseudocode.js
@@ -0,0 +1,36 @@
+/* Given two numbers
+Calculate the sum of two numbers
+Return the result */
+
+/* Given an array of strings
+
+Create a new empty string
+Iterate through the array of strings
+  - For each iteration, append the value to the empty string */
+
+/* Given an array of integers
+
+Create a new empty array
+Iterate through the array of integers
+  - Append the first integer into the empty array
+  - Skip over the next integer 
+  - Repeat the process until the entire array has been iterated through */
+
+/* Given a string and the target character
+
+Create a counter starting at 0
+Split the string into an array
+Iterate over the array 
+  - Check the current character against the target character
+    - If they match, add one to the counter
+    - If they do not, skip
+  - When the counter reaches 3, return the index value of the current iteration */
+
+  /* Given two equally sized arrays of integers
+
+  Create a new empty array
+  Begin iterating against the length of the arrays
+    - For each iteration, append a number from the first array to the empty array
+    - Then append a number from the second array to the empty array */
+
+    
