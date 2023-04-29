7db9ff9a027f869083a20961ae09dbd8d3140a52
Christian <christian.james.fisk@gmail.com>
2023-04-29 19:01:35 +1000

    Wrapping in functions

--- Courses/JS101/Lesson 2/loan_calculator.js
+++ Courses/JS101/Lesson 2/loan_calculator.js
@@ -19,24 +19,61 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function isInvalidNumber(number) {
+  return number.trim() === '' ||
+         Number(number) < 0   ||
+         Number.isNaN(Number(number));
+}
+
+function getLoanAmount() {
+  prompt("Please enter the total loan amount");
+  let loanAmount = readline.question();
+
+  while (isInvalidNumber(loanAmount)) {
+    prompt('Must enter a positive number');
+    loanAmount = readline.question();
+  }
+  return Number(loanAmount);
+}
+
+function getMonthlyInterestRate() {
+  prompt("Please enter the interest rate per annum");
+  prompt("Example: 5 for 5% or 2.5 for 2.5%");
+  let interestRate = readline.question();
+
+  while (isInvalidNumber(interestRate)) {
+    prompt('Must enter a positive number without a % sign');
+    interestRate = readline.question();
+  }
+  let yearlyInterestRate = parseFloat(interestRate) / 100;
+  let monthlyInterestRate = yearlyInterestRate / 12;
+  return monthlyInterestRate;
+}
+
+function getLoanDuration() {
+  prompt("Finally, please enter the loan duration in years");
+  let loanDurationYears = readline.question();
+
+  while (isInvalidNumber(loanDurationYears)) {
+    prompt("Please enter a positive number");
+  }
+  let loanDurationMonths = loanDurationYears * 12;
+  return loanDurationMonths;
+}
+
+function calculateMonthlyPayment(loanAmount, interestRate, loanDuration) {
+  return loanAmount * (interestRate /
+         (1 - Math.pow((1 + monthlyInterest), (-loanDuration))));
+}
 
 prompt("Welcome to the loan Calculator!");
+console.log('--------------------------------------');
 
-prompt("Please enter the total loan amount");
-let loanAmount = Number(readline.question());
+let loanAmount = getLoanAmount();
+let monthlyInterestRate = getMonthlyInterestRate();
+let loanDurationMonths = getLoanDuration();
 
-prompt("Please enter the interest rate per annum");
-let interestRate = parseFloat(readline.question());
-
-prompt("Finally, please enter the loan duration in years");
-let loanDurationYears = Number(readline.question());
-
-let interestRateYearly = interestRate / 100;
-let interestRateMonthly = interestRateYearly / 12;
-let loanDurationMonths = loanDurationYears * 12;
-
-let monthlyPayment = loanAmount * (interestRateMonthly /
-    (1 - Math.pow((1 + interestRateMonthly), (-loanDurationMonths))));
+let monthlyPayment = calculateMonthlyPayment(loanAmount, monthlyInterestRate, loanDurationMonths);
 
 prompt(`Your weekly interest payment is $${(parseFloat(monthlyPayment) / 4).toFixed(2)}`);
 prompt(`Your monthly interest payment is $${(monthlyPayment).toFixed(2)}`);
